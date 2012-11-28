5a69e2fbb7a863f9e9dec796127e8d85193f2adf
Thomas Tsou <tom@tsou.cc>
2012-11-28 12:04:52 -0500

    transceiver: sigproc: add NEON float-integer conversions
    Initial implementation for quad-word conversions for converstions
    to and from complex single precision floating point to complex
    16-bit integer representations.
    
    Signed-off-by: Thomas Tsou <tom@tsou.cc>

---
+++ Transceiver52M/sigproc/convert.h
@@ -0,0 +1,7 @@
+#ifndef _CONVERT_H_
+#define _CONVERT_H_
+
+void convert_flt32_int16(short *shrt, float *flt, int len);
+void convert_int16_flt32(float *flt, short *shrt, int len);
+
+#endif /* _CONVERT_H_ */
